78ecc9a03b009a4cac884a97640e1ca5bb6aa0fe
Admira1B <vreizenbuk@mail.ru>
2023-03-30 22:08:30 +0700

    PetOwner Data Transfer Objects are added

---
+++ VeterinaryApi/Dtos/PetOwnerDtos/GetPetOwnerDto.cs
@@ -0,0 +1,14 @@
+﻿using VeterinaryApi.Models;
+
+namespace VeterinaryApi.Dtos.PetOwnerDtos
+{
+    public class GetPetOwnerDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public List<Pet> Pets { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ VeterinaryApi/Dtos/PetOwnerDtos/UpdatePetOwnerDto.cs
@@ -0,0 +1,13 @@
+﻿using VeterinaryApi.Models;
+
+namespace VeterinaryApi.Dtos.PetOwnerDtos
+{
+    public class UpdatePetOwnerDto
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public List<Pet> Pets { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
